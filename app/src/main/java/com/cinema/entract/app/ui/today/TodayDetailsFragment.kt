a60037e72b63902383c53634ed317c813fd54f90
BAIGET Stéphane (i-BP) <stephane.baiget@i-bp.fr>
2018-09-14 13:54:18 +0200

    Rename packages.

---
+++ app/src/main/java/com/cinema/entract/app/ui/today/TodayDetailsFragment.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018 Stéphane Baiget
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.cinema.entract.app.ui.today
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.cinema.entract.app.R
+import com.cinema.entract.app.ext.load
+import com.cinema.entract.app.ui.base.BaseFragment
+import org.jetbrains.anko.find
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+class TodayDetailsFragment : BaseFragment() {
+
+    private val viewModel by sharedViewModel<TodayViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_today_details, container, false)
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.selectedMovie?.let {
+            with(view) {
+                find<ImageView>(R.id.cover).load(it.coverUrl)
+                find<TextView>(R.id.title).text = it.title
+                find<TextView>(R.id.director).text = it.director
+                find<TextView>(R.id.cast).text = it.cast
+                find<TextView>(R.id.year).text = it.yearOfProduction
+                find<TextView>(R.id.duration).text = it.duration
+                find<TextView>(R.id.genre).text = it.genre
+                find<TextView>(R.id.synopsis).text = it.synopsis
+            }
+        }
+    }
+
+    companion object {
+        fun newInstance() = TodayDetailsFragment()
+    }
+}

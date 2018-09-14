1b6ec731591ab855c8fcc7e82a71970598b1d97a
BAIGET Stéphane (i-BP) <stephane.baiget@i-bp.fr>
2018-09-14 13:54:18 +0200

    Rename packages.

---
+++ app/src/main/java/com/cinema/entract/app/ui/base/BaseViewModel.kt
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2018 Stéphane Baiget
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.cinema.entract.app.ui.base
+
+import androidx.annotation.CallSuper
+import androidx.lifecycle.ViewModel
+import com.cinema.entract.app.utils.CoroutinesUtils.Companion.tryCatch
+import com.cinema.entract.app.utils.CoroutinesUtils.Companion.tryCatchFinally
+import com.cinema.entract.app.utils.CoroutinesUtils.Companion.tryFinally
+import com.cinema.entract.data.interactor.BaseUseCase
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+
+
+open class BaseViewModel<out UseCase : BaseUseCase>(val useCase: UseCase) : ViewModel() {
+
+    private val asyncJobs = mutableListOf<Job>()
+
+    override fun onCleared() {
+        super.onCleared()
+        useCase.cleanup()
+        cleanup()
+    }
+
+    @CallSuper
+    @Synchronized
+    protected fun launchAsync(block: suspend CoroutineScope.() -> Unit) {
+        val job: Job = launch(UI) { block() }
+        asyncJobs.add(job)
+        job.invokeOnCompletion { asyncJobs.remove(job) }
+    }
+
+    @Synchronized
+    protected fun launchAsyncTryCatch(
+        tryBlock: suspend CoroutineScope.() -> Unit,
+        catchBlock: suspend CoroutineScope.(Throwable) -> Unit,
+        handleCancellationExceptionManually: Boolean = false
+    ) {
+        launchAsync { tryCatch(tryBlock, catchBlock, handleCancellationExceptionManually) }
+    }
+
+    @Synchronized
+    protected fun launchAsyncTryCatchFinally(
+        tryBlock: suspend CoroutineScope.() -> Unit,
+        catchBlock: suspend CoroutineScope.(Throwable) -> Unit,
+        finallyBlock: suspend CoroutineScope.() -> Unit,
+        handleCancellationExceptionManually: Boolean = false
+    ) {
+        launchAsync {
+            tryCatchFinally(
+                tryBlock,
+                catchBlock,
+                finallyBlock,
+                handleCancellationExceptionManually
+            )
+        }
+    }
+
+    @Synchronized
+    protected fun launchAsyncTryFinally(
+        tryBlock: suspend CoroutineScope.() -> Unit,
+        finallyBlock: suspend CoroutineScope.() -> Unit,
+        suppressCancellationException: Boolean = false
+    ) {
+        launchAsync { tryFinally(tryBlock, finallyBlock, suppressCancellationException) }
+    }
+
+    @CallSuper
+    @Synchronized
+    protected fun cancelAllAsync() {
+        val asyncJobsSize = asyncJobs.size
+        if (asyncJobsSize > 0) {
+            for (i in asyncJobsSize - 1 downTo 0) {
+                asyncJobs[i].cancel()
+            }
+        }
+    }
+
+    @CallSuper
+    @Synchronized
+    open fun cleanup() {
+        cancelAllAsync()
+    }
+}
